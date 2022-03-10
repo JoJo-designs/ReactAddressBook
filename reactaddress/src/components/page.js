237723dec9b383c5d13645136b27f9d2389f9f8b
JoJo-designs <jodiladouceur.design@gmail.com>
2022-03-10 11:23:05 -0500

    continued edits to the page

--- reactaddress/src/components/page.js
+++ reactaddress/src/components/page.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Page () {
+
+    const [addnew, setAddNew] = useState(false)
+    
+    const [newData, setNewData] = useState({
+        name: '',
+        phone: '',
+        email: ''
+    })
+
+    const handleChange = (event) => {
+        const {target} = event;
+        const inputType = target.name;
+        const inputValue = target.value;
+        console.log(inputType, inputValue)
+    }
+
     return(
-        <h1>This is a page!</h1>
+        <div>
+        <h1>Address Book</h1>
+        {addnew ?
+        <div>
+            <input placeholder='name'
+            value={newData.name}
+            name="name"
+            type="text"
+            onChange={handleChange}
+            />
+
+            <input placeholder='phone'/>
+
+            <input placeholder='email'/>
+
+            <button onClick={() => setAddNew(false)}>submit</button>
+        </div> 
+        : <button onClick={() => setAddNew(true)}>Add New</button>}
+        </div>
     )
 }
